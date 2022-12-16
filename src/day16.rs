6763b226aff266c72900411ff24bfa8b90f4c9a2
Jlg <julien.le-guen@kaizen-solutions.net>
2022-12-16 14:01:53 +0100

    day 16 graph

--- src/day16.rs
+++ src/day16.rs
@@ -1,5 +1,5 @@
 use parse_display::{Display, FromStr};
-//use std::collections::HashMap;
+use std::collections::HashMap;
 //use std::fmt;
 //use std::str::FromStr;
 use std::string::ParseError;
@@ -10,38 +10,73 @@ use std::string::ParseError;
 //use serde_json::{Number, Value};
 //use std::cmp::Ordering;
 //use std::iter::zip;
-use petgraph::{graphmap::UnGraphMap};
+use petgraph::graph::{Graph, NodeIndex, UnGraph};
+use petgraph::prelude::*;
+use petgraph::visit::{depth_first_search, Control, DfsEvent};
 
 // ---------------------------------------------------------------------------
-#[derive(Debug, Hash, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
+#[derive(Debug, Hash, Ord, PartialOrd, Eq, PartialEq, Clone)]
 pub struct Valve {
-    name: &str,
     flow: usize,
+    index: NodeIndex,
 }
 
 #[derive(Debug)]
 pub struct ParsedValve {
-    name: &str,
+    name: String,
     flow: usize,
     neighbours: Vec<String>,
 }
 
-impl Valve {
-    fn from(v: &ParsedValve) -> Self {
-        Valve { name: v.name.clone(), flow: v.flow }
-    }
-}
+impl Valve {}
 
-
+#[derive(Debug)]
 pub struct Map {
-    valves: Vec<Valve>,
-    graph: UnGraphMap<Valve, ()>,
+    valves: HashMap<String, Valve>,
+    graph: UnGraph<usize, usize>,
 }
 
 impl Map {
-    fn new(valves: &Vec<ParsedValve>) -> Self {
-        let graph: UnGraphMap<Valve, ()> = UnGraphMap::new();
-        Map { valves: valves.iter().map(|v| Valve::from(v)).collect(), graph }
+    fn new(input: &Vec<ParsedValve>) -> Self {
+        let mut graph: UnGraph<usize, usize> = Graph::new_undirected();
+        let mut valves: HashMap<String, Valve> = HashMap::new();
+        // Nodes
+        for parsed in input {
+            let i = graph.add_node(parsed.flow);
+            valves.insert(
+                parsed.name.clone(),
+                Valve {
+                    flow: parsed.flow,
+                    index: i,
+                },
+            );
+        }
+        // Edges
+        for parsed in input {
+            let index = valves.get(&parsed.name).expect("Valve").index;
+            for n in parsed.neighbours.iter() {
+                let i = valves.get(n).expect("Neighbour").index;
+                graph.add_edge(index, i, 1);
+            }
+        }
+
+        Map { valves, graph }
+    }
+
+    // Remove nodes with flow 0
+    fn simplify(&mut self) {
+        let mut graph: UnGraph<usize, usize> = Graph::new_undirected();
+        let root = self.valves["AA"].index;
+        let mut bfs = Bfs::new(&self.graph, root);
+        let mut dist = 0;
+        depth_first_search(&self.graph, Some(root), |event| {
+            match event {
+                DfsEvent::TreeEdge(u, v) => {
+                    dist += 1;
+                }
+                DfsEvent::BackEdge
+            }
+        });
     }
 }
 
@@ -50,12 +85,25 @@ fn parse_line(line: &str) -> ParsedValve {
     let (v, t) = line.split_once(';').expect("Line");
     //  "Valve BB has flow rate=13"
     let elems: Vec<&str> = v.split_ascii_whitespace().collect();
-    let name = elems[1];
-    let flow = elems[4].split_once('=').expect("Flow").1.parse::<usize>().unwrap();
+    let name = elems[1].to_string();
+    let flow = elems[4]
+        .split_once('=')
+        .expect("Flow")
+        .1
+        .parse::<usize>()
+        .unwrap();
     //  " tunnels lead to valves CC, AA"
-    let neighbours = t.split_ascii_whitespace().skip(4).map(|s| String::from(s.trim_matches(','))).collect();
+    let neighbours = t
+        .split_ascii_whitespace()
+        .skip(4)
+        .map(|s| String::from(s.trim_matches(',')))
+        .collect();
 
-    ParsedValve{ name, flow, neighbours }
+    ParsedValve {
+        name,
+        flow,
+        neighbours,
+    }
 }
 
 #[aoc_generator(day16)]
@@ -65,8 +113,10 @@ pub fn input_generator(input: &str) -> Map {
         valves.push(parse_line(&line));
     }
 
-    println!("{:#?}", valves);
-    Map::new(&valves)
+    let map = Map::new(&valves);
+
+    println!("{:?}", map);
+    map
 }
 
 // ---------------------------------------------------------------------------
@@ -108,6 +158,5 @@ Valve JJ has flow rate=21; tunnel leads to valve II";
         assert_eq!(1651, part1(&input));
     }
     #[test]
-    fn test_part2() {
-    }
+    fn test_part2() {}
 }
